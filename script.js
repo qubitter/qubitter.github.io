e38ce40bf9bb5d267bdace83c598348caf9b391f
Charlie B <11893617+qubitter@users.noreply.github.com>
2024-03-15 21:42:46 -0400

    Upgraded js file

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function getSong() {
+function loadSong() {
   var xhttp = new XMLHttpRequest();
   var pre = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/';
   var post = ' width="80%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("song").innerHTML = pre + this.responseText + post;
+    if (this.readyState == 4) {
+      switch (this.status) {
+        case 200:
+          document.getElementById("song").innerHTML = pre + this.responseText + post;
+          break;
+        case 204:
+          document.getElementById("song").innerHTML = "Nothing right now! Check back later."
+          break;
+        case 503:
+          document.getElementById("song").innerHTML = "Obscurify is down! Check back later."
+          break;
+      }
     }
   };
-  xhttp.open("GET", "http://140.82.51.99:80", true);
+  xhttp.open("GET", "https://127.0.0.1/spotify", true);
+  // xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
   xhttp.send();
 }
+
